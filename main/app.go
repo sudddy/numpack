7b342998625d3d487fb5026355e7547d1ef03ce9
Sudharsan S <sudharsans@Sudharsans-MacBook-Air.local>
2021-10-26 13:48:15 +1300

    file structure

---
+++ main/app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sudddy/numpack/calc"
+)
+
+func main() {
+	fmt.Println("Calcualtion")
+	result := calc.add(1,2)
+	fmt.Println("result, ", result)
+}
